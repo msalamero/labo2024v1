631c6a953146f08d36a78b3f19fcc88c6cc9bade
msalamero <martin.salamero@gmial.com>
2024-05-03 20:50:49 -0300

    base2

--- src/workflow-01/ms_501_workflow_Base2.r
+++ src/workflow-01/ms_501_workflow_Base2.r
@@ -318,18 +318,18 @@ corrida_guantesblancos_202109 <- function( pnombrewf, pvirgen=FALSE )
 {
   if( -1 == exp_wf_init( pnombrewf, pvirgen) ) return(0) # linea fija
 
-  DT_incorporar_dataset_default( "DT0001", "competencia_2024.csv.gz")
-  CA_catastrophe_default( "CA0001", "DT0001" )
+  DT_incorporar_dataset_default( "DT0002", "competencia_2024.csv.gz")
+  CA_catastrophe_default( "CA0002", "DT0002" )
 
-  DR_drifting_guantesblancos( "DR0001", "CA0001" )
-  FE_historia_guantesblancos( "FE0001", "DR0001" )
+  DR_drifting_guantesblancos( "DR0002", "CA0002" )
+  FE_historia_guantesblancos( "FE0002", "DR0002" )
 
-  TS_strategy_guantesblancos_202109( "TS0001", "FE0001" )
+  TS_strategy_guantesblancos_202109( "TS0002", "FE0002" )
 
-  HT_tuning_guantesblancos( "HT0001", "TS0001" )
+  HT_tuning_guantesblancos( "HT0002", "TS0002" )
 
   # El ZZ depente de HT y TS
-  ZZ_final_guantesblancos( "ZZ0001", c("HT0001","TS0001") )
+  ZZ_final_guantesblancos( "ZZ0002", c("HT0002","TS0002") )
 
 
   exp_wf_end( pnombrewf, pvirgen ) # linea fija
